90b934d58948204dfaff060dee259784c9910ce1
Eric Chen <ejc196@gmail.com>
2020-11-18 11:21:39 -0800

    Sorted Histogram by Value, Fixed html links

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
 var stemmer = require('stemmer')
 
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/WordBars.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;  
+    }  
+});  
+server.listen(3000);  
 
-console.log(stemmer("detestable"))
-// https://www.npmjs.com/package/stemmer
-//       var stemmer = require('stemmer')
-//       console.log(stemmer("detestable"))
 
 
-var toSplit = "Detestable stemmer removal effective";
-var toStem = toSplit.split(" ");
-console.log(toStem);
-for(s = 0; s < toStem.length; s++){
-    toStem[s] = stemmer(toStem[s]);
-}
+// var stemmer = require('stemmer')
 
-console.log(toStem);
-string = toStem.join(" ");
-console.log(string);
+
+// console.log(stemmer("detestable"))
+// // https://www.npmjs.com/package/stemmer
+// //       var stemmer = require('stemmer')
+// //       console.log(stemmer("detestable"))
+
+
+// var toSplit = "Detestable stemmer removal effective";
+// var toStem = toSplit.split(" ");
+// console.log(toStem);
+// for(s = 0; s < toStem.length; s++){
+//     toStem[s] = stemmer(toStem[s]);
+// }
+
+// console.log(toStem);
+// string = toStem.join(" ");
+// console.log(string);
